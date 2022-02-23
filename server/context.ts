5d19ec9cd26ff230d30da97768410b3d3f99ce20
Pascal Martineau <pascal@lewebsimple.ca>
2022-02-23 09:00:54 -0500

    feat: provide auth in GraphQL execution context

--- server/context.ts
+++ server/context.ts
@@ -1,9 +1,16 @@
-import { prisma } from "../prisma/client";
+import type { ExecutionContext } from "graphql-helix";
+import { parse } from "cookie-es";
+import { prisma } from "~/prisma/client";
+import type { AuthState } from "~/utils/jwt";
+import { decodeJwt, jwtCookieName } from "~/utils/jwt";
 
 export type Context = {
+  auth: AuthState;
   prisma: typeof prisma;
 };
 
-export async function contextFactory(): Promise<Context> {
-  return { prisma };
-}
+export const contextFactory = async ({ request }: ExecutionContext): Promise<Context> => {
+  const cookies: Record<string, string> = parse((request.headers as { cookie: string }).cookie || "");
+  const token = cookies[jwtCookieName] || "";
+  return { auth: decodeJwt(token), prisma };
+};
