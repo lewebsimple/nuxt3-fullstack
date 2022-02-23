6cd79c6b80b924c4b5cd2dc68aa1f5ef9d55d642
Pascal Martineau <pascal@lewebsimple.ca>
2022-02-23 08:58:58 -0500

    feat: JWT helpers

---
+++ utils/jwt.ts
@@ -0,0 +1,41 @@
+import type { ServerResponse } from "http";
+import type { User, UserRole } from "@prisma/client";
+import type { CookieSerializeOptions } from "cookie-es";
+import { setCookie } from "h3";
+import type { SignOptions } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+// Type safe authentication state validation
+export interface AuthState {
+  user: null | {
+    id: number;
+    role: UserRole;
+  };
+}
+
+export const validateAuthState = (authState: AuthState): AuthState => ({
+  user: authState?.user ? (({ id, role }) => ({ id, role }))(authState.user) : null,
+});
+
+// Decode and validate authentication state payload from (string) token
+const jwtSecretKey = process.env.JWT_SECRET_KEY || "jwtsecretkey";
+
+export const decodeJwt = (token: string): AuthState => {
+  try {
+    const authState = jwt.verify(token, jwtSecretKey) as AuthState;
+    return validateAuthState(authState);
+  } catch (error) {
+    return { user: null };
+  }
+};
+
+// Encode authentication state as JWT cookie in server response
+export const jwtCookieName = process.env.JWT_COOKIE_NAME || "jwt";
+const jwtSignOptions: SignOptions = { expiresIn: "2h" };
+const jwtCookieOptions: CookieSerializeOptions = { path: "/", httpOnly: true };
+
+export const setAuthState = (user: User | null, res: ServerResponse): AuthState => {
+  const authState = validateAuthState({ user });
+  setCookie(res, jwtCookieName, jwt.sign(authState, jwtSecretKey, jwtSignOptions), jwtCookieOptions);
+  return authState;
+};
