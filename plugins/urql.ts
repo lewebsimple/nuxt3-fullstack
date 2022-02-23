2bb193abf6b048655829bd27686885c30fe38d2e
Pascal Martineau <pascal@lewebsimple.ca>
2022-02-23 08:51:58 -0500

    feat: setup @urql/vue SSR exchange

--- plugins/urql.ts
+++ plugins/urql.ts
@@ -1,10 +1,33 @@
-import urql from "@urql/vue";
+import urql, { cacheExchange, dedupExchange, fetchExchange, ssrExchange } from "@urql/vue";
 import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const { graphqlApiURL } = useRuntimeConfig();
 
+  // Create SSR exchange
+  const ssr = ssrExchange({
+    isClient: process.client,
+  });
+
+  // Extract SSR payload once app is rendered on the server
+  if (process.server) {
+    nuxtApp.hook("app:rendered", () => {
+      nuxtApp.payload?.data && (nuxtApp.payload.data.urql = ssr.extractData());
+    });
+  }
+
+  // Restore SSR payload once app is created on the client
+  if (process.client) {
+    nuxtApp.hook("app:created", () => {
+      nuxtApp.payload?.data && ssr.restoreData(nuxtApp.payload.data.urql);
+    });
+  }
+
+  // Custom exchanges
+  const exchanges = [dedupExchange, cacheExchange, ssr, fetchExchange];
+
   nuxtApp.vueApp.use(urql, {
     url: graphqlApiURL,
+    exchanges,
   });
 });
