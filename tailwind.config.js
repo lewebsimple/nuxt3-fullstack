f783fc5f290bd4bf255217056e9747e7f26f82ae
Pascal Martineau <pascal@lewebsimple.ca>
2022-02-28 13:27:54 -0500

    fix: no need for @tailwindcss/aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp"), require("@tailwindcss/typography")],
 };
