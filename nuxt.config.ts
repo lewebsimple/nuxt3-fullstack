f1e6577ae512f265c9f3e155081a33da0cfb3f1b
Pascal Martineau <pascal@lewebsimple.ca>
2022-03-16 20:11:24 -0400

    refactor: css in main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ["@urql/vue"],
   },
+  css: ["~/assets/main.css"],
   buildModules: ["@nuxtjs/tailwindcss", "@formkit/nuxt"],
   publicRuntimeConfig: {
     graphqlApiURL: process.env.GRAPHQL_API_URL || "http://localhost:3000/api/graphql",
